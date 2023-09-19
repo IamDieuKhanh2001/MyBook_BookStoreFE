ef6a1bfee25ef470a1b11711fd563b375a0d45c0
IamDieuKhanh2001 <quachdieukhanh@gmail.com>
2023-09-19 19:02:30 +0700

    flash sale slider

--- src/components/home/FlashSale/FlashSale.tsx
+++ src/components/home/FlashSale/FlashSale.tsx
@@ -4,6 +4,7 @@ import styles from './FlashSale.module.scss'
 import FlashSaleHeader from './FlashSaleHeader/FlashSaleHeader'
 import ProductList from '@/components/product/ProductList/ProductList'
 import Link from 'next/link'
+import FlashSaleBody from './FlashSaleBody/FlashSaleBody'
 
 const FlashSale = () => {
     return (
@@ -11,10 +12,10 @@ const FlashSale = () => {
             <div className="container-xxl pt-5">
                 <FlashSaleHeader initialHours={10} initialMinutes={30}/>
                 <div className='row pb-4'>
-                    <ProductList />
+                    <FlashSaleBody />
                 </div>
                 <div className="col-12 text-center">
-                    {/* Go to product list page  */}
+                    {/* Go to flash sale list page  */}
                     <Link className="btn btn-primary rounded-pill py-3 px-5" href="">
                         Xem thêm
                     </Link>
