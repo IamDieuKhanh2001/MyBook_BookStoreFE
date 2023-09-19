8874a9f66139b3a5a4d3b9a0af658d1c3a4faaa6
IamDieuKhanh2001 <quachdieukhanh@gmail.com>
2023-09-19 16:37:07 +0700

    Login register formik

--- src/components/authentication/LoginForm/LoginForm.tsx
+++ src/components/authentication/LoginForm/LoginForm.tsx
@@ -1,49 +1,152 @@
 'use client'
 import React, { useState } from 'react'
 import styles from './LoginForm.module.scss'
+import { Alert, AlertTitle, Typography } from '@mui/material'
+import { useSearchParams } from 'next/navigation'
+import * as Yup from 'yup';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { toast } from "react-toastify";
+import { signIn } from 'next-auth/react'
 
+interface FormValues {
+    email: string;
+    password: string;
+}
 const LoginForm = () => {
+    const searchParams = useSearchParams();
+    const isError = searchParams && searchParams.get('error') === "CredentialsSignin" ? true : false;
+    const isSessionExpired = searchParams ? searchParams.get("isSessionExpired") === "true" : false;
+    const [isLoading, setIsLoading] = useState(false)
     const [showPassword, setShowPassword] = useState(false)
 
     const handleShowPassword = () => {
         setShowPassword(!showPassword)
     }
+    const initialValues: FormValues = {
+        email: '',
+        password: '',
+    };
+    const validationSchema = Yup.object({
+        email: Yup.string()
+            .required("email not be empty"),
+        password: Yup.string().required("Password not be empty"),
+    });
+
+    const handleSubmit = async (values: FormValues) => {
+        try {
+            // Xử lý logic khi form được submit
+            toast.warning("Login with " + values.email + " " + values.password)
+            // const result = await signIn("credentials", {
+            //     email: values?.email,
+            //     password: values?.password,
+            //     redirect: true,
+            //     callbackUrl: "/",
+            // });
+            setIsLoading(true)
+            setTimeout(() => {
+                toast.success("Xin chào " + values.email)
+                // Đoạn mã bạn muốn thực thi sau 5 giây
+                setIsLoading(false)
+            }, 5000); // 5000 milliseconds = 5 giây
+        } catch (e) {
+            console.log(e)
+            setIsLoading(false)
+            toast.error("Can not Login with" + values.email)
+        }
+    };
+
     return (
         <div className={styles.loginFormContainer}>
-            <div className={styles.inputBox}>
-                <label>Email</label>
-                <div className={styles.inputGroup}>
-                    <input type="text" className={styles.textBox} placeholder='Nhập Email' />
-                </div>
-            </div>
-            <div className={styles.inputBox}>
-                <label>Mật khẩu</label>
-                <div className={styles.inputGroup}>
-                    <input
-                        type={showPassword === false ? 'password' : 'text'}
-                        className={styles.textBox}
-                        placeholder='Nhập Mật khẩu'
-                    />
-                    <span className={styles.textBoxShowText} onClick={handleShowPassword}>
-                        {showPassword === false ? 'Hiện' : 'Ẩn'}
-                    </span>
-                </div>
-            </div>
-            <div className={styles.inputBox}>
-                <div className={styles.forgetPass}>
-                    <span>Quên mật khẩu?</span>
-                </div>
-            </div>
-            <div className={styles.inputBox}>
-                <div className={styles.btnBox}>
-                    <button title="Đăng nhập" className={styles.btnLogin}>
-                        <span>Đăng nhập</span>
-                    </button>
-                    <button title="Đăng nhập" className={styles.btnFacebook}>
-                        <span>Đăng nhập với facebook</span>
-                    </button>
-                </div>
-            </div>
+            {isError === true && (
+                <Alert severity="error" >
+                    <AlertTitle>Login Error!</AlertTitle>
+                    Something went wrong — <strong>Please try again!</strong>
+                </Alert>
+            )}
+            {isSessionExpired === true && (
+                <Alert severity="warning" >
+                    <AlertTitle>Session Has expired!</AlertTitle>
+                    Your session not available — <strong>Please login again!</strong>
+                </Alert>
+            )}
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+            >
+                {({ setFieldValue, isValid, handleChange, errors, touched, isSubmitting }) => (
+                    <Form>
+                        <div className={styles.inputBox}>
+                            <label>Email</label>
+                            <div className={styles.inputGroup}>
+                                <Field
+                                    id={"email"}
+                                    name={"email"}
+                                    type="text"
+                                    className={styles.textBox}
+                                    placeholder='Nhập Email'
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            {errors.email && touched.email && (
+                                <p className={styles.msgFieldError}>
+                                    *{errors.email}
+                                </p>
+                            )}
+                        </div>
+                        <div className={styles.inputBox}>
+                            <label>Mật khẩu</label>
+                            <div className={styles.inputGroup}>
+                                <Field
+                                    id={"password"}
+                                    name={"password"}
+                                    type={showPassword === false ? 'password' : 'text'}
+                                    className={styles.textBox}
+                                    placeholder='Nhập Mật khẩu'
+                                    onChange={handleChange}
+                                />
+                                <span className={styles.textBoxShowText} onClick={handleShowPassword}>
+                                    {showPassword === false ? 'Hiện' : 'Ẩn'}
+                                </span>
+                            </div>
+                            {errors.password && touched.password && (
+                                <p className={styles.msgFieldError}>
+                                    *{errors.password}
+                                </p>
+                            )}
+                        </div>
+                        <div className={styles.inputBox}>
+                            <div className={styles.forgetPass}>
+                                <span>Quên mật khẩu?</span>
+                            </div>
+                        </div>
+                        <div className={styles.inputBox}>
+                            <div className={styles.btnBox}>
+                                <button
+                                    type='submit'
+                                    title="Đăng nhập"
+                                    className={styles.btnLogin}
+                                    disabled={isLoading || !isValid}// disabled nếu isLoading = true hoặc isValid = false
+                                >
+                                    {isLoading ? (
+                                        <span>
+                                            <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                            Loading...
+                                        </span>
+                                    ) : (
+                                        <span>
+                                            Đăng nhập
+                                        </span>
+                                    )}
+                                </button>
+                                <button title="Đăng nhập" className={styles.btnFacebook}>
+                                    <span>Đăng nhập với facebook</span>
+                                </button>
+                            </div>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }
--- src/components/authentication/RegisterForm/RegisterForm.tsx
+++ src/components/authentication/RegisterForm/RegisterForm.tsx
@@ -1,56 +1,158 @@
 'use client'
 import React, { useState } from 'react'
 import styles from './RegisterForm.module.scss'
+import * as Yup from 'yup';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { toast } from "react-toastify";
+import { Typography } from '@mui/material';
 
+interface FormValues {
+    email: string;
+    password: string;
+    retypePassword: string;
+}
 const RegisterForm = () => {
+    const [isLoading, setIsLoading] = useState(false)
     const [showPassword, setShowPassword] = useState(false)
 
     const handleShowPassword = () => {
         setShowPassword(!showPassword)
     }
 
+    const initialValues: FormValues = {
+        email: '',
+        password: '',
+        retypePassword: '',
+    };
+    const getCharacterValidationError = (str: string) => {
+        return `Your password must have at least 1 ${str} character`;
+    };
+    const validationSchema = Yup.object({
+        email: Yup.string()
+            .required("email not be empty"),
+        password: Yup.string()
+            .required("Password not be empty")
+            .min(5, "Password must have at least 5 characters")
+            // different error messages for different requirements
+            .matches(/[0-9]/, getCharacterValidationError("digit"))
+            .matches(/[a-z]/, getCharacterValidationError("lowercase"))
+            .matches(/[A-Z]/, getCharacterValidationError("uppercase")),
+        retypePassword: Yup.string()
+            .oneOf([Yup.ref("password")], "Passwords does not match")
+            .required("Retype password not be empty"),
+    });
+
+    const handleSubmit = async (values: FormValues) => {
+        try {
+            // Xử lý logic khi form được submit
+            console.log(values)
+            toast.warning("Register with " + values.email + " " + values.password)
+            setIsLoading(true)
+            setTimeout(() => {
+                toast.success("Xin chào " + values.email)
+                // Đoạn mã bạn muốn thực thi sau 5 giây
+                setIsLoading(false)
+            }, 5000); // 5000 milliseconds = 5 giây
+        } catch (e) {
+            console.log(e)
+            setIsLoading(false)
+            toast.error("Can not Register with" + values.email)
+        }
+    };
+
     return (
         <>
             <div className={styles.loginFormContainer}>
-                <div className={styles.inputBox}>
-                    <label>Email</label>
-                    <div className={styles.inputGroup}>
-                        <input type="text" className={styles.textBox} placeholder='Nhập Email' />
-                    </div>
-                </div>
-                <div className={styles.inputBox}>
-                    <label>Mật khẩu</label>
-                    <div className={styles.inputGroup}>
-                        <input
-                            type={showPassword === false ? 'password' : 'text'}
-                            className={styles.textBox}
-                            placeholder='Nhập Mật khẩu'
-                        />
-                        <span className={styles.textBoxShowText} onClick={handleShowPassword}>
-                            {showPassword === false ? 'Hiện' : 'Ẩn'}
-                        </span>
-                    </div>
-                </div>
-                <div className={styles.inputBox}>
-                    <label>Nhập lại mật khẩu</label>
-                    <div className={styles.inputGroup}>
-                        <input
-                            type={showPassword === false ? 'password' : 'text'}
-                            className={styles.textBox}
-                            placeholder='Nhập lại Mật khẩu'
-                        />
-                        <span className={styles.textBoxShowText} onClick={handleShowPassword}>
-                            {showPassword === false ? 'Hiện' : 'Ẩn'}
-                        </span>
-                    </div>
-                </div>
-                <div className={styles.inputBox}>
-                    <div className={styles.btnBox}>
-                        <button title="Đăng nhập" className={styles.btnLogin}>
-                            <span>Đăng Ký</span>
-                        </button>
-                    </div>
-                </div>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    {({ setFieldValue, isValid, handleChange, errors, touched, isSubmitting }) => (
+                        <Form>
+                            <div className={styles.inputBox}>
+                                <label>Email</label>
+                                <div className={styles.inputGroup}>
+                                    <Field
+                                        id={"email"}
+                                        name={"email"}
+                                        type="text"
+                                        className={styles.textBox}
+                                        placeholder='Nhập Email'
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                {errors.email && touched.email && (
+                                    <p className={styles.msgFieldError}>
+                                        *{errors.email}
+                                    </p>
+                                )}
+                            </div>
+                            <div className={styles.inputBox}>
+                                <label>Mật khẩu</label>
+                                <div className={styles.inputGroup}>
+                                    <Field
+                                        id={"password"}
+                                        name={"password"}
+                                        type={showPassword === false ? 'password' : 'text'}
+                                        className={styles.textBox}
+                                        placeholder='Nhập Mật khẩu'
+                                        onChange={handleChange}
+                                    />
+                                    <span className={styles.textBoxShowText} onClick={handleShowPassword}>
+                                        {showPassword === false ? 'Hiện' : 'Ẩn'}
+                                    </span>
+                                </div>
+                                {errors.password && touched.password && (
+                                    <p className={styles.msgFieldError}>
+                                        *{errors.password}
+                                    </p>
+                                )}
+                            </div>
+                            <div className={styles.inputBox}>
+                                <label>Nhập lại mật khẩu</label>
+                                <div className={styles.inputGroup}>
+                                    <Field
+                                        id={"retypePassword"}
+                                        name={"retypePassword"}
+                                        type={showPassword === false ? 'password' : 'text'}
+                                        className={styles.textBox}
+                                        placeholder='Nhập lại Mật khẩu'
+                                        onChange={handleChange}
+                                    />
+                                    <span className={styles.textBoxShowText} onClick={handleShowPassword}>
+                                        {showPassword === false ? 'Hiện' : 'Ẩn'}
+                                    </span>
+                                </div>
+                                {errors.retypePassword && touched.retypePassword && (
+                                    <p className={styles.msgFieldError}>
+                                        *{errors.retypePassword}
+                                    </p>
+                                )}
+                            </div>
+                            <div className={styles.inputBox}>
+                                <div className={styles.btnBox}>
+                                    <button
+                                        type='submit'
+                                        className={styles.btnRegister}
+                                        disabled={isLoading || !isValid}// disabled nếu isLoading = true hoặc isValid = false
+                                    >
+                                        {isLoading ? (
+                                            <span>
+                                                <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                                Loading...
+                                            </span>
+                                        ) : (
+                                            <span>
+                                                Đăng Ký
+                                            </span>
+                                        )}
+                                    </button>
+                                </div>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         </>
     )
